b0f0bbf279e96f519dd6d33420f84fb97c4fd8fc
khushinarang <knaran02@students.cs.ubc.ca>
2024-03-09 22:41:15 -0800

    added parsed data from api output

--- recommendations.py
+++ recommendations.py
@@ -12,10 +12,19 @@ def generate_text(prompt, temp=0):
       if event.event_type == "text-generation":
           print(event.text, end='')
 
-user_input_mood = "happy"
+user_input_mood = ["happy", "excited"]
 user_input_health = "pain in knees"
-user_input_duration = "5"
 
-prompt = f"""Recommend 3 to 4 yoga poses for a person with health condition as {user_input_health} and  with mood {user_input_mood}. Insert an underscore symbol between the description and the steps."""
+prompt = f"""I want to know 3 yoga poses if I have a health condition as {user_input_health} and my mood is {user_input_mood}. I want their use based on the mood and health condition. I also want the steps to perform the yoga pose. I want it in an enumerated format like: 1. POSE_NAME: Use: Steps: STEPS"""
 
-generate_text(prompt, temp=0.5)
+cohere_recommendations_text = generate_text(prompt, temp=0.5)
+
+
+l_1 = cohere_recommendations_text.split("1.")
+l_2 = cohere_recommendations_text.split("2.")
+l_3 = cohere_recommendations_text.split("3.")
+first_pose = l_1[0]
+second_pose = l_2[0]
+third_pose = l_3[0]
+
+
