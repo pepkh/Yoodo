eb2a0084f3fde09d1472ee8730b1ae2f795b91bc
khushinarang <knaran02@students.cs.ubc.ca>
2024-03-09 23:14:54 -0800

    added variables of each pose

--- recommendations.py
+++ recommendations.py
@@ -7,24 +7,30 @@ def generate_text(prompt, temp=0):
     message=prompt,
     temperature=temp,
     stream=True)
+  
+  generated_text = []
 
   for event in response:
       if event.event_type == "text-generation":
-          print(event.text, end='')
+        generated_text.append(event.text)
+  return ''.join(generated_text)
 
 user_input_mood = ["happy", "excited"]
 user_input_health = "pain in knees"
 
 prompt = f"""I want to know 3 yoga poses if I have a health condition as {user_input_health} and my mood is {user_input_mood}. I want their use based on the mood and health condition. I also want the steps to perform the yoga pose. I want it in an enumerated format like: 1. POSE_NAME: Use: Steps: STEPS"""
 
+# generate_text(prompt, temp=0.5)
+
 cohere_recommendations_text = generate_text(prompt, temp=0.5)
+# print(cohere_recommendations_text)
 
-
-l_1 = cohere_recommendations_text.split("1.")
-l_2 = cohere_recommendations_text.split("2.")
-l_3 = cohere_recommendations_text.split("3.")
-first_pose = l_1[0]
-second_pose = l_2[0]
-third_pose = l_3[0]
+# retrieving the poses as strings 
+l_1 = cohere_recommendations_text.split(":")
+l_2 = cohere_recommendations_text.split(":")
+l_3 = cohere_recommendations_text.split(":")
+first_pose = l_1[0].split(".")[1]
+second_pose = l_2[0].split(".")[1]
+third_pose = l_3[0].split(".")[1]
 
 
