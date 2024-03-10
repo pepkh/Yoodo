7a6ceb63f8ef108f21ae6905637163656d362142
aicarol <lilin202026@gmail.com>
2024-03-10 00:17:04 -0800

    add logo

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,12 +1,11 @@
 import './NavBar.css'
+import logo from '../images/Yoodo_logo.png'
 
 export const NavBar = () => {
     return (
         <div className="navbar">
             {/* icon and main options */}
-            <div className="">
-            <p>YOOGO -yoga for your mood-</p>
-            </div>
+            <img src={logo} alt="" width={220} height={150}></img>
         </div>
     );
 };
